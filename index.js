099ba18bc6f0126adeb713951fc113366f16b4da
Anchu0327 <92390439+Anchu0327@users.noreply.github.com>
2025-02-28 00:50:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{console.log('hey!', new Date()})
+app.get('/',(req,res)=>{console.log('hey!', new Date())})
 
 setInterval(async ()=>{
     const resp = await axios.get('https://alive-6bh2.onrender.com');
