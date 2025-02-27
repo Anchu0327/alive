ecaba2364d6346dbca2477bb8c729e7121584c7c
Anchu0327 <92390439+Anchu0327@users.noreply.github.com>
2025-02-28 00:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/',(req,res)=>{console.log('hey!', new Date()})
+
 setInterval(async ()=>{
-    // const resp = await axios.get('https://project-stories.onrender.com/keep-alive');
-    // console.log(`Got keep-alive for stories! - `,(resp.data))
+    const resp = await axios.get('https://alive-6bh2.onrender.com');
+    console.log(`Got keep-alive! - `,(resp.data))
     const resp2 = await axios.get('https://vanaj-backend-dev.onrender.com/');
     console.log(`Got keep-alive for vanaj! - `,(resp2.data))
 }, 14*1000*60)
